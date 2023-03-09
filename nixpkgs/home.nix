427d24ecad08903d55c223f36b0573ad8769dca3
arpanetus <arpanetus@protonmail.com>
2023-03-09 20:46:55 +0600

    Add Nix caches

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -27,10 +27,15 @@ in
 
       # Haskell.nix binary cache.
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+        "cache.iog.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+        "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
       ];
       substituters = [
         "https://cache.iog.io"
+        "https://iohk.cachix.org"
+        "https://cache.nixos.org/"
       ];
     };
   };
